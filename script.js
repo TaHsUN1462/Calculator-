1db8374cc8369065239b74a395bf90f7c4dc19ce
TaHsUN1462 <135030521+TaHsUN1462@users.noreply.github.com>
2024-08-13 17:01:16 +0600

    update script.js

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 let display = document.getElementById('display');
+let result = document.getElementById('result');
 let buttons = document.querySelectorAll("button");
 let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button =>{
-  button.addEventListener('click',(e) =>{
-    if (e.target.innerHTML == "=") {
+let array = Array.from(buttons);
+array.forEach(button => {
+  button.addEventListener("click",(e) =>{
+    if (e.target.innerHTML == '=') {
       string = eval(string);
+      display.value = "";
+      result.value = string;
+    }else if(e.target.innerHTML == 'AC'){
+      string = "";
+      result.value = string;
       display.value = string;
-    }else if(e.target.innerHTML == "AC"){
-      string = ""
-      display.value = string;
-    }else if(e.target.innerHTML == "DEL") {
+    }else if(e. target.innerHTML == 'DEL'){
       string = string.substring(0, string.length-1);
       display.value = string;
     }else{
       string += e.target.innerHTML;
       display.value = string;
     }
-  } )
+  })
 })
